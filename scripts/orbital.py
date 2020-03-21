60605ee42bbdb0b5fc1fa41739a2b8184322546f
Lucas Walter <wsacul@gmail.com>
2020-03-21 07:50:20 -0700

    Now have edge wall

--- scripts/orbital.py
+++ scripts/orbital.py
@@ -10,14 +10,19 @@ import numpy as np
 # meters
 width_m = 35e6
 diameter_m = 4.5e9
+# edgewall_m = 2e6
 edgewall_m = 2e6
 scale_down = 1.0 / width_m
 
 angle_span = 10.0
-angle_steps = 5.0
+angle_steps = 16.0
 
 with open('cyl_section.obj', 'w') as obj:
     obj.write('o orbital_section\n')
+    half_width = width_m * scale_down * 0.5
+    radius = diameter_m * scale_down * 0.5
+    wall_radius = (diameter_m * 0.5 - edgewall_m) * scale_down
+    print('{} {}'.format(radius, wall_radius))
     angle_inc = angle_span / angle_steps
     for count, angle_deg in enumerate(np.arange(-angle_span * 0.5,
                                                 angle_span * 0.5,
@@ -25,22 +30,26 @@ with open('cyl_section.obj', 'w') as obj:
         # print('{} {}'.format(count, angle_deg))
         angle = math.radians(angle_deg + angle_inc * 0.5)
 
-        radius = diameter_m * scale_down * 0.5
-        half_width = width_m * scale_down * 0.5
+        # offset x so numbers are close to zero
+        wall_x = wall_radius * math.cos(angle) - radius
+        wall_y = wall_radius * math.sin(angle)
 
-        # offset so numbers are close to zero
         x = radius * math.cos(angle) - radius
         y = radius * math.sin(angle)
 
         z = -half_width
+        obj.write('v {} {} {}\n'.format(wall_x, wall_y, z))
         obj.write('v {} {} {}\n'.format(x, y, z))
 
         z = half_width
         obj.write('v {} {} {}\n'.format(x, y, z))
+        obj.write('v {} {} {}\n'.format(wall_x, wall_y, z))
 
+    pts_per_segment = 4
     for i in range(count):
-        v0 = i * 2 + 1
-        v1 = v0 + 1
-        v2 = (i + 1) * 2 + 2
-        v3 = v2 - 1
-        obj.write('f {}//{} {}//{} {}//{} {}//{}\n'.format(v0, v0, v1, v1, v2, v2, v3, v3))
+        for j in range(pts_per_segment - 1):
+            v0 = i * pts_per_segment + j + 1
+            v1 = v0 + 1
+            v2 = (i + 1) * pts_per_segment + j + 2
+            v3 = v2 - 1
+            obj.write('f {}//{} {}//{} {}//{} {}//{}\n'.format(v0, v0, v1, v1, v2, v2, v3, v3))
